14084a56fdc1be94bcdb231fd1a49094ed7a0dc7
brromney <122390309+brromney@users.noreply.github.com>
2023-01-12 14:42:18 -0700

    now!

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.vpc_id
+  vpc_id             = module.vpc.vpc_id
   subnets            = module.vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
