a99da36c61fa7c42826cae383b7d7c8cd02402b5
brromney <122390309+brromney@users.noreply.github.com>
2023-01-12 14:34:37 -0700

    maybe this time

--- main.tf
+++ main.tf
@@ -84,7 +84,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.16.2"
 
-  name   = "blog_new"
+  name   = "blog"
   vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
