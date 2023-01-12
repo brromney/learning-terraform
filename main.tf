9ce3dbc09bcc78dbf26c76dc7f0babff91625bed
brromney <122390309+brromney@users.noreply.github.com>
2023-01-12 15:16:08 -0700

    remove

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -39,7 +39,7 @@ module "autoscaling" {
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_groups_arns
-  security_groups      = [module.blog_sg.security_group_id]
+  security_groups     = [module.blog_sg.security_group_id]
 
   image_id            = data.aws_ami.app_ami.id
   instance_type       = var.instance_type
@@ -63,12 +63,6 @@ module "blog_alb" {
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
     }
   ]
 
